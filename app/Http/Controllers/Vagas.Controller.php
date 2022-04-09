1efa818f5c4b0839a4c8b6a2c40bf4f04951492a
Luís Tchitue <ls04af@gmail.com>
2022-04-09 16:38:10 +0100

    adiciona uma rota para as vagas disponiveis

---
+++ app/Http/Controllers/Vagas.Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+//use App\Http\Controllers\Controller;
+
+class VagasController extends Controller
+{
+    public function vagas() {
+        return 'vagas da nossa da app';
+    }
+}
