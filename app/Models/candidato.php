7d24ea130379a608ed93d749278a934746631f50
Anildo-Marques23 <82910699+Anildo-Marques23@users.noreply.github.com>
2022-04-27 23:47:22 +0100

    Encaminhando dados post do Angular para a base de dados

---
+++ app/Models/candidato.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+//use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class candidato extends Model
+{
+    //use HasFactory;
+    public $timestamps = false;
+//    protected $fillable = ['Nome', 'BI', 'Email', 'nacionalidade', 'Telefone', 'Nivel_academico ', 
+  //  'Anos_de_experiencia'];
+}
